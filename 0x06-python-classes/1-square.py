985da9bf9d7779d61af2cf0c969b62ba4a893f37
BlackjackXXI <oussama.bouaoued@gmail.com>
2023-12-19 13:05:42 -0800

    fixed files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # 0-square.py by Ehoneah Obed
 """Defines a square """
+
+
 class Square:
     """Represents a square"""
+
     def __init__(self, size):
         """Initializing this square class
         Args: size - represnets the size of the square defined
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # 0-square.py by Ehoneah Obed
 """A module that defines a square """
+
+
 class Square:
     """A class that represents a square"""
+
     def __init__(self, size=0):
         """Initializing this square class
         Args:
@@ -16,11 +19,15 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+
         self.__size = size
+
     @property
     def size(self):
         """Retrieves size of square"""
+
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -28,9 +35,11 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def area(self):
         """
         Calculate area of the square
         Returns: The square of the size
         """
+
         return (self.__size ** 2)
