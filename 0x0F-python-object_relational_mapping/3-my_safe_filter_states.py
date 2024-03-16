e6fa94063530033c2211f498b9406f02b2c035bc
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-16 13:12:34 -0700

    fixed attributes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,12 @@
 x#!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa, safe from injection
-"""
+ script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 
+"""
 import MySQLdb
 from sys import argv
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name=%s\
                     ORDER BY states.id ASC", (argv[4],))
