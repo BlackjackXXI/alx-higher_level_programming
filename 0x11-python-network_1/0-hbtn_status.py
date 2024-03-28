f8f77e7cfd0ddce7f4628b308f3f1afc844e14b0
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-03-28 23:07:48 +0000

    gg2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""write a Python script that fetches https://intranet.hbtn.io/status using the package urllib"""
+"""Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
