90e52a670cd07676b366422b5d5ce8df575f46c1
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-04-23 23:50:04 +0000

    1233

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-const lien = process.argv[2];
 
-request(lien, (err, res) => {
-  if (err) console.log(err);
-  console.log('code:', res.statusCode);
+let url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
