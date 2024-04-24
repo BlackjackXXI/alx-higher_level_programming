8fc929815e97d6e7342dedc0cab2ffb446269b1d
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-04-24 00:30:28 +0000

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
+
 const request = require('request');
 const util = require('util');
-const id = process.argv[2];
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 const prequest = util.promisify(request);
 
 (async () => {
   try {
-    const body = (await prequest(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true })).body;
-    for (const url of body.characters) {
-      const body = (await prequest(url, { json: true })).body;
-      console.log(body.name);
+    const filmData = (await prequest(apiUrl, { json: true })).body;
+    for (const characterUrl of filmData.characters) {
+      const characterData = (await prequest(characterUrl, { json: true })).body;
+      console.log(characterData.name);
     }
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.error(error);
   }
 })();
