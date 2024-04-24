196887eaa17f8a155bd668d76589c9f756d08bb9
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-04-24 00:16:42 +0000

    done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  const results = {};
-  for (const task of body) {
-    if (task.completed) {
-      results[task.userId] = (results[task.userId] || 0) + 1;
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error('Oops! Something went wrong:', err);
+  } else {
+    const completedTasks = {};
+    for (const task of body) {
+      if (task.completed) {
+        completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+      }
     }
+    console.log(completedTasks);
   }
-  console.log(results);
 });
