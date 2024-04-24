e446ddd5883cfb3901aff1f4891c74328d3f1145
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-04-24 00:01:25 +0000

    1233

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, (err, res, body) => {
-  if (err) console.log(err);
-  const count = body.split('/people/18/').length - 1;
-  console.log(count);
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body);
+    let count = 0;
+    filmsData.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
 });
