64aaef31f6da40141626e3450963e6042808b0dd
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-04-24 00:24:10 +0000

    done

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-
 if (process.argv.length < 3) {
     console.error('Please provide a movie ID as an argument.');
-    process.exit(1);}
+    process.exit(1);
+}
 const movieId = process.argv[2];
 const movieUrl = `https://swapi.co/api/films/${movieId}/`;
 request(movieUrl, (error, response, body) => {
     if (error) {
         console.error('Error fetching movie data:', error);
-        process.exit(1);}
+        process.exit(1);
+    }
     const movieData = JSON.parse(body);
     const charactersUrls = movieData.characters;
     charactersUrls.forEach((url, index) => {
@@ -18,7 +19,8 @@ request(movieUrl, (error, response, body) => {
                 console.error(`Error fetching character data for character ${index + 1}:`, error);
             } else {
                 const characterData = JSON.parse(body);
-                console.log(characterData.name);}
+                console.log(characterData.name);
+            }
         });
     });
 });
